840cc6432d46d649d4e4ad480359457aff17367a
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-04-23 13:36:39 -0300

    - Created Services and Repositories.

--- Node/src/server.ts
+++ Node/src/server.ts
@@ -1,11 +1,10 @@
-import express  from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
-app.get('/', (request,response)=>{
-    return response.json({message:'helloWorld'});
-})
-
-app.listen(3333, ()=>{
-    console.log('🕋 server online.');
+app.listen(3333, () => {
+  console.log('🕋 server online.');
 });
