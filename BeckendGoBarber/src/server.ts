edb20df68ea5adb71048830e5dd57ffe1f0946a1
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-05-18 15:14:31 -0300

    - adiciona cors ao projeto.

--- BeckendGoBarber/src/server.ts
+++ BeckendGoBarber/src/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
@@ -8,6 +9,7 @@ import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
