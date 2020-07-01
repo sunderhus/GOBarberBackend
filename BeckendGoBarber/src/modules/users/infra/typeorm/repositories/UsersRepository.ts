775420cfdd1c01f936994e6fa262486ca063d622
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-07-01 16:25:30 -0300

    -feature: Listagem de prestadores de serviços. - tests: testes unitários para a listagem de prestadores de serviços.

--- BeckendGoBarber/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ BeckendGoBarber/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,7 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Not } from 'typeorm';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IFindAllProvidersDTO from '@modules/appointments/dtos/IFindAllProvidersDTO';
 
 class UsersRepository implements IUsersRepository {
   private ormRepository: Repository<User>;
@@ -20,6 +21,24 @@ class UsersRepository implements IUsersRepository {
     const user = await this.ormRepository.findOne(id);
 
     return user;
+  }
+
+  public async findAllProviders({
+    exceptUserId,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let users: User[];
+
+    if (exceptUserId) {
+      users = await this.ormRepository.find({
+        where: {
+          id: Not(exceptUserId),
+        },
+      });
+    } else {
+      users = await this.ormRepository.find();
+    }
+
+    return users;
   }
 
   public async create(data: ICreateUserDTO): Promise<User> {
