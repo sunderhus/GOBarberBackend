3d5ab9be55c5670136112b9d69e632c42f6ecd9b
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-07-03 19:10:56 -0300

    - feature: Criação de listagem de horários livrem em um dia de agendamento do prestador de serviços. - tests : Testes unitários para horários habilitados em um dia de trabalho do prestador de serviços.

--- BeckendGoBarber/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ BeckendGoBarber/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,9 +1,10 @@
 import { uuid } from 'uuidv4';
-import { isEqual, getMonth, getYear } from 'date-fns';
+import { isEqual, getMonth, getYear, getDate } from 'date-fns';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 
 class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
@@ -36,6 +37,23 @@ class AppointmentsRepository implements IAppointmentsRepository {
     const appointments = await this.appointments.filter(appointment => {
       return (
         appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year
+      );
+    });
+    return appointments;
+  }
+
+  public async findAllInDayFromProvider({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const appointments = await this.appointments.filter(appointment => {
+      return (
+        appointment.provider_id === provider_id &&
+        getDate(appointment.date) === day &&
         getMonth(appointment.date) + 1 === month &&
         getYear(appointment.date) === year
       );
---
+++ BeckendGoBarber/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -0,0 +1,54 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderDayAvailabilityService from './ListProviderDayAvailabilityService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderDayAvailabilityService: ListProviderDayAvailabilityService;
+
+describe('ListProviderDayAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderDayAvailabilityService = new ListProviderDayAvailabilityService(
+      fakeAppointmentsRepository
+    );
+  });
+
+  it('should be able to list all provider day availability for appointments', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: '123123',
+      date: new Date(2020, 4, 20, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: '123123',
+      date: new Date(2020, 4, 20, 11, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: '123123',
+      date: new Date(2020, 4, 20, 12, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
+    });
+
+    const dailyAvailability = await listProviderDayAvailabilityService.execute({
+      provider_id: '123123',
+      day: 20,
+      month: 5,
+      year: 2020,
+    });
+
+    expect(dailyAvailability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 11, available: false },
+        { hour: 12, available: false },
+        { hour: 13, available: true },
+        { hour: 14, available: true },
+      ])
+    );
+  });
+});
