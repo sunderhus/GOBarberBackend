54da83052028cfe7bbe8bda7d83fb5a62a0f4440
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-07-08 15:33:57 -0300

    - feature: Listagem de agendamentos em um dia específico para o prestador de serviços logado.

--- BeckendGoBarber/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ BeckendGoBarber/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,13 +2,17 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsControler from '../controllers/AppointmentsControler';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 
 const appointmentsController = new AppointmentsControler();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
---
+++ BeckendGoBarber/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,38 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderDayAvailabilityService from './ListProviderDayAvailabilityService';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointmentsService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository
+    );
+  });
+
+  it('should be able to list all provider appointments in a day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    const availability = await listProviderAppointments.execute({
+      day: 20,
+      month: 5,
+      year: 2020,
+      provider_id: 'provider',
+    });
+
+    expect(availability).toEqual([appointment1, appointment2]);
+  });
+});
