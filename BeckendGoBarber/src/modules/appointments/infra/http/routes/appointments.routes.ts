890bbeaaa76324c757bbdfa4f95d13c0199dcd79
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-07-13 10:32:47 -0300

    feature: - adiciona celebrate ao projeto - Adiciona na rota agendamentos, validação do body para chamada.

--- BeckendGoBarber/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ BeckendGoBarber/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsControler from '../controllers/AppointmentsControler';
 import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
@@ -11,7 +11,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmentsController.create
+);
 
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
